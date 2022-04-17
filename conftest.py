f75f2fe23304a408bd977ba8df132a86fc2d3090
Rymarava Natalia <rymareva.nat@gmail.com>
2022-04-17 17:38:13 +0300

    Lesson 9. Turned off captcha.

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import json
 import pytest
 import os.path
 import importlib
+import ftputil
 
 from fixture.application import Application
 
@@ -19,13 +20,17 @@ def load_config(file):
 
 
 @pytest.fixture
-def app(request):
+def app(request, config):
     global fixture
     browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target"))['web']
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, baseURL=web_config['baseURL'])
+        fixture = Application(browser=browser, baseURL=config['web']['baseURL'])
     return fixture
+
+
+@pytest.fixture(scope='session')
+def config(request):
+    return load_config(request.config.getoption("--target"))
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -35,6 +40,32 @@ def stop(request):
 
     request.addfinalizer(finalizer)
     return fixture
+
+
+@pytest.fixture(scope='session', autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+
+    def fin():
+       restore_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    request.addfinalizer(fin)
+
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile('config_inc.php.bak'):
+            remote.remove('config_inc.php.bak')
+        if remote.path.isfile('config_inc.php'):
+            remote.rename('config_inc.php', 'config_inc.php.bak')
+        remote.upload(os.path.join((os.path.dirname(__file__)), 'resources/config_inc.php'), 'config_inc.php')
+
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile('config_inc.php.bak'):
+            if remote.path.isfile('config_inc.php'):
+                remote.remove('config_inc.php')
+            remote.rename('config_inc.php.bak', 'config_inc.php')
 
 
 def pytest_addoption(parser):
